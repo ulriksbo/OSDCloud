037e2d2a3183a6b61addc71ee31312fc99bfd469
ulriksbo <ulriksbo@users.noreply.github.com>
2022-08-15 22:28:16 +0200

    Add files via upload

---
+++ UpdateFiles.ps1
@@ -0,0 +1,7 @@
+## update Test-PSCloudScriptGit.ps1 with VE version
+Write-Host -ForegroundColor Cyan "Updating Test-PSCloudScriptGit.ps1"
+$version = (get-childitem "$env:SystemDrive\Program Files\WindowsPowerShell\Modules\OSD\" | Select -Last 1).name
+$modulepath = "$env:SystemDrive\Program Files\WindowsPowerShell\Modules\OSD\$version\Public"
+remove-item "$modulepath\Test-PSCloudScriptGit.ps1"
+(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ulriksbo/OSDCloud/main/Test-PSCloudScriptGit.ps1',"$modulepath\Test-PSCloudScriptGit.ps1")
+
