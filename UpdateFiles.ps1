c9666f6792d959ecfae1058ffc727a932ef3ab66
ulriksbo <ulriksbo@users.noreply.github.com>
2022-08-15 22:30:42 +0200

    Update UpdateFiles.ps1

--- UpdateFiles.ps1
+++ UpdateFiles.ps1
@@ -2,6 +2,6 @@
 Write-Host -ForegroundColor Cyan "Updating Test-PSCloudScriptGit.ps1"
 $version = (get-childitem "$env:SystemDrive\Program Files\WindowsPowerShell\Modules\OSD\" | Select -Last 1).name
 $modulepath = "$env:SystemDrive\Program Files\WindowsPowerShell\Modules\OSD\$version\Public"
-remove-item "$modulepath\Test-PSCloudScriptGit.ps1"
+remove-item "$modulepath\Test-PSCloudScriptGit.ps1" -Force
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ulriksbo/OSDCloud/main/Test-PSCloudScriptGit.ps1',"$modulepath\Test-PSCloudScriptGit.ps1")
 
